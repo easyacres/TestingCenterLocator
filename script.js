cbecf31aff1e4b5158cd1375a96dce533bcb07a6
Santiago Solana <santiago.sjs@gmail.com>
2020-07-30 13:20:41 -0400

    github pages big fix

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $("#goButton").on("click", function (event) {
     event.preventDefault();
     var stateCode = $("#stateList").val();
     var zipCode = $("#zip").val();
-    window.location.replace("/results.html?zip=" + zipCode + "&state=" + stateCode);
+    window.location.replace("TestingCenterLocator/results.html?zip=" + zipCode + "&state=" + stateCode);
    
 
 });
